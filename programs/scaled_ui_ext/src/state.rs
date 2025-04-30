1463445635b05ca001c78cc68d56807dd7b8a9be
Jordan Hall <oighty6@gmail.com>
2025-04-30 12:30:46 -0500

    chore: name and other cleanup

--- programs/scaled_ui_ext/src/state.rs
+++ programs/scaled_ui_ext/src/state.rs
@@ -19,7 +19,7 @@ pub struct ExtGlobal {
 }
 
 #[constant]
-pub const MINT_AUTHORITY_SEED: &[u8] = b"ext_mint_authority";
+pub const MINT_AUTHORITY_SEED: &[u8] = b"mint_authority";
 
 #[constant]
 pub const M_VAULT_SEED: &[u8] = b"m_vault";
