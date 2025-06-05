8b8b3361ae2180f61ad8aeb2ed9b17f70026830c
Jordan Hall <oighty6@gmail.com>
2025-06-05 16:20:23 -0500

    refactor: claim fees based on accrued fee principal

--- programs/m_ext/src/instructions/claim_fees.rs
+++ programs/m_ext/src/instructions/claim_fees.rs
@@ -8,7 +8,10 @@ use crate::{
     errors::ExtError,
     state::{ExtGlobal, EXT_GLOBAL_SEED, MINT_AUTHORITY_SEED, M_VAULT_SEED},
     utils::{
-        conversion::{amount_to_principal_down, principal_to_amount_up, sync_multiplier},
+        conversion::{
+            amount_to_principal_down, principal_to_amount_down, principal_to_amount_up,
+            sync_multiplier,
+        },
         token::mint_tokens,
     },
 };
@@ -75,7 +78,7 @@ impl ClaimFees<'_> {
     pub fn handler(ctx: Context<Self>) -> Result<()> {
         // Sync the multiplier before allowing any collateral withdrawals
         let signer_bump = ctx.accounts.global_account.ext_mint_authority_bump;
-        let multiplier: f64 = sync_multiplier(
+        let multiplier = sync_multiplier(
             &mut ctx.accounts.ext_mint,
             &mut ctx.accounts.global_account,
             &ctx.accounts.m_earn_global_account,
@@ -85,25 +88,12 @@ impl ClaimFees<'_> {
             &ctx.accounts.ext_token_program,
         )?;
 
-        // Calculate the required collateral, rounding down to be conservative
-        // This amount will always be greater than what is required in the check_solvency function
-        // since it allows a rounding error of up to 2e-6
-        let required_m = principal_to_amount_up(ctx.accounts.ext_mint.supply, multiplier)?;
-
-        // Excess M is the amount of M in the vault above the amount needed to fully collateralize the extension
-        let vault_m = ctx.accounts.vault_m_token_account.amount;
-
-        let excess = vault_m
-            .checked_sub(required_m)
-            .ok_or(ExtError::InsufficientCollateral)?; // This shouldn't underflow, but we check for safety
-
-        let excess_principal = amount_to_principal_down(excess, multiplier)?;
-
-        // Only transfer a positive amount of excess
-        if excess_principal > 0 {
+        // Mint new tokens to the recipient if there are any fees to claim
+        let accrued_fee_principal = ctx.accounts.global_account.accrued_fee_principal;
+        if accrued_fee_principal > 0 {
             mint_tokens(
                 &ctx.accounts.recipient_ext_token_account,
-                excess_principal,
+                accrued_fee_principal,
                 &ctx.accounts.ext_mint,
                 &ctx.accounts.ext_mint_authority,
                 &[&[
@@ -113,10 +103,13 @@ impl ClaimFees<'_> {
                 &ctx.accounts.ext_token_program,
             )?;
 
+            // Reset the accrued fee principal after minting
+            ctx.accounts.global_account.accrued_fee_principal = 0;
+
             emit!(FeesClaimed {
                 recipient_token_account: ctx.accounts.recipient_ext_token_account.key(),
-                amount: excess,
-                principal: excess_principal,
+                amount: principal_to_amount_down(accrued_fee_principal, multiplier)?,
+                principal: accrued_fee_principal,
             });
         }
 
