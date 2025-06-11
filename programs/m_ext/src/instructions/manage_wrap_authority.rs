b0d300205a00f29e82118967aaa4bdf1a9ff3c4e
Jordan Hall <oighty6@gmail.com>
2025-06-11 16:50:56 -0500

    feat: change m_ext wrap authorities list to be dynamic

---
+++ programs/m_ext/src/instructions/manage_wrap_authority.rs
@@ -0,0 +1,116 @@
+use anchor_lang::prelude::*;
+
+use crate::{
+    errors::ExtError,
+    state::{ExtGlobal, EXT_GLOBAL_SEED},
+};
+
+#[derive(Accounts)]
+pub struct AddWrapAuthority<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [EXT_GLOBAL_SEED],
+        has_one = admin @ ExtError::NotAuthorized,
+        bump = global_account.bump,
+        realloc = ExtGlobal::size(global_account.wrap_authorities.len() + 1),
+        realloc::payer = admin,
+        realloc::zero = false,
+    )]
+    pub global_account: Account<'info, ExtGlobal>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl AddWrapAuthority<'_> {
+    // This instruction allows the admin to add a wrap authority to the global account.
+    // The new wrap authority must not already exist in the list.
+
+    pub fn validate(&self, new_wrap_authority: Pubkey) -> Result<()> {
+        // Validate that the new wrap authority is not already in the list
+        if self
+            .global_account
+            .wrap_authorities
+            .contains(&new_wrap_authority)
+        {
+            return err!(ExtError::InvalidParam);
+        }
+
+        Ok(())
+    }
+
+    #[access_control(ctx.accounts.validate(new_wrap_authority))]
+    pub fn handler(ctx: Context<Self>, new_wrap_authority: Pubkey) -> Result<()> {
+        // Update the wrap authority at the specified index
+        ctx.accounts
+            .global_account
+            .wrap_authorities
+            .push(new_wrap_authority);
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct RemoveWrapAuthority<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [EXT_GLOBAL_SEED],
+        has_one = admin @ ExtError::NotAuthorized,
+        bump = global_account.bump,
+    )]
+    pub global_account: Account<'info, ExtGlobal>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl RemoveWrapAuthority<'_> {
+    // This instruction allows the admin to remove a wrap authority from the global account.
+    // The wrap authority must exist in the list.
+
+    pub fn validate(&self, wrap_authority: Pubkey) -> Result<()> {
+        // Validate that the wrap authority exists in the list
+        if !self
+            .global_account
+            .wrap_authorities
+            .contains(&wrap_authority)
+        {
+            return err!(ExtError::InvalidParam);
+        }
+
+        Ok(())
+    }
+
+    #[access_control(ctx.accounts.validate(wrap_authority))]
+    pub fn handler(ctx: Context<Self>, wrap_authority: Pubkey) -> Result<()> {
+        msg!(
+            "current length: {}",
+            ctx.accounts.global_account.wrap_authorities.len()
+        );
+
+        // Remove the specified wrap authority
+        ctx.accounts
+            .global_account
+            .wrap_authorities
+            .retain(|&x| !x.eq(&wrap_authority));
+
+        msg!(
+            "new length: {}",
+            ctx.accounts.global_account.wrap_authorities.len()
+        );
+
+        // Reallocate the account to remove the empty space without erasing the other data
+        let new_size = ExtGlobal::size(ctx.accounts.global_account.wrap_authorities.len());
+        ctx.accounts
+            .global_account
+            .to_account_info()
+            .realloc(new_size, false)?;
+
+        Ok(())
+    }
+}
