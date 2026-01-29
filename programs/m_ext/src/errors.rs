5789e43e2d594567934c0b6a550406cb004f51ac
Ian Harvey <16181275+ith-harvey@users.noreply.github.com>
2026-01-29 09:44:55 -0600

    fix: removed unused error

--- programs/m_ext/src/errors.rs
+++ programs/m_ext/src/errors.rs
@@ -34,13 +34,10 @@ pub enum ExtError {
     InvalidTokenProgram,
     #[msg("Vault token account frozen.")]
     VaultFrozen,
-
     #[msg("Asset not allowed for wrapping.")]
     AssetNotAllowed,
     #[msg("Asset cap would be exceeded.")]
     AssetCapExceeded,
-    #[msg("Insufficient M backing for unwrap.")]
-    InsufficientMBacking,
     #[msg("Cannot set cap for M token.")]
     CannotCapMToken,
     #[msg("Insufficient asset backing for replacement.")]
